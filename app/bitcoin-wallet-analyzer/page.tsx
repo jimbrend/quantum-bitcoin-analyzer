4000b9ce83d5698e17a7b42a0ff0bf76f134248a
Jim Brrr <jimbrrr@Mac.lan>
2025-06-05 03:51:30 -0400

    feat: integrate bitcoin wallet analyzer quiz into main project

---
+++ app/bitcoin-wallet-analyzer/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import WalletAnalyzer from "@/components/wallet-analyzer"
+
+export default function BitcoinWalletAnalyzerPage() {
+  return (
+    <div className="min-h-screen bg-black">
+      <WalletAnalyzer />
+    </div>
+  )
+} 
